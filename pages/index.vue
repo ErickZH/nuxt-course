d1c0af296692f115317b951b909e9a8f4ba2db3c
Erick Zarate <zaraterick@outlook.com>
2018-12-11 11:39:29 -0600

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+    <h1>Hola mundo</h1>
+</template>
+
+<script>
+import axios from 'axios'
+
+import env from '../config/env'
+
+export default {
+    created () {
+        axios.get(`${env.endpoint}/albums`).then((response) => {
+            console.log(response.data);
+        })
+    }
+}
+</script>
